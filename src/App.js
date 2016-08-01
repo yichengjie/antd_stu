d7f6adc3ca9a09c26fe7e13075dc93240c2e9407
yichengjie <626659321@qq.com>
2016-08-01 21:42:23 +0800

    demo date picker

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { NICE, SUPER_NICE } from './colors';
+import { DatePicker,message } from 'antd';
+//import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
 
 class Counter extends Component {
   constructor(props) {
@@ -28,11 +30,23 @@ class Counter extends Component {
 }
 
 export class App extends Component {
+  constructor(props){
+    super(props) ;
+    this.state = {
+        date:''
+    } ;
+  }
+  handleChange(date){
+    message.info('你选择的日期是 : ' + date.toString()) ;
+    this.setState({date}) ;
+  }
   render() {
     return (
       <div>
         <Counter increment={1} color={NICE} />
         <Counter increment={5} color={SUPER_NICE} />
+        <DatePicker onChange = {value => this.handleChange(value)} />
+        <div style={{ marginTop: 20 }}>当前日期：{this.state.date.toString()}</div>
       </div>
     );
   }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,10 +22,16 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+       test: /\.js$/,
+       loaders: ['babel'],
+       include: path.join(__dirname, 'src')
+     },
+     {
+        test: /\.css$/,
+        loader: "style!css"
+     },
+  ]
   }
 };
